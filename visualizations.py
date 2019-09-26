ed719e28b3b9ade3ae9a6fbb3219cf301b9b55a7
John Morrison <jjosephmorrison@gmail.com>
2019-09-26 01:27:53 -0400

    adding int

--- visualizations.py
+++ visualizations.py
@@ -153,6 +153,8 @@ def gauge_pred(df):
     drop_columns = ['campaignName', 'description', 'user_id']
     data_df.drop(columns = drop_columns, inplace=True)
 
+    data_df['monetaryGoal'] = int(data_df['monetaryGoal'])
+
     probability = predict_proba(model, data_df)
 
     fig = go.Figure(go.Indicator(
