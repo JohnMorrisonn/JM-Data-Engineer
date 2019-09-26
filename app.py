f82c78dd7c11159f472a519620674ce629dca12f
John Morrison <jjosephmorrison@gmail.com>
2019-09-25 23:24:31 -0400

    goal

--- app.py
+++ app.py
@@ -227,12 +227,12 @@ def predict():
     # Final output dict
     output = {'results': int(model_result[0]),
             'custom_stats': {
-                'raising_more_success' : custom_results[0],
+                'raising_more_success' : goal,
                 'category_success' : custom_results[1],
-                'category_average' : int(custom_results[2]),
-                'average_duration' : int(custom_results[3]),
-                'average_backers' : int(custom_results[4]),
-                'average_over' : int(custom_results[5])
+                'category_average' : custom_results[2],
+                'average_duration' : custom_results[3],
+                'average_backers' : custom_results[4],
+                'average_over' : custom_results[5]
             }
     }
     return jsonify(output)
